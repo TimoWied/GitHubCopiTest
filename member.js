33f75939108ce62310eb074bf321c77c2e9509b4
TimoWied <147503978+TimoWied@users.noreply.github.com>
2023-10-12 07:22:35 +0000

    Copilot t commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+  var skills = [
+    { name: 'HTML', level: 3 },
+    { name: 'CSS', level: 3 },
+    { name: 'JS', level: 2 },
+    { name: 'PHP', level: 1 },
+    { name: 'MySQL', level: 1 },
+    { name: 'Python', level: 1 }
+  ];
+  return skills;
+}
